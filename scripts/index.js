a5ff11d92ceecc1e770fa393cd45698f8125ee9b
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-17 13:39:42 +0200

    Completato il fetch delle immagini, generazione dinamica delle card con title, date e foto. Aggiornati gli stili di gallery, card e font per allineamento al layout richiesto.

--- scripts/index.js
+++ scripts/index.js
@@ -7,19 +7,26 @@ fetch(API_URL)
     .then(response => {
         return response.json();
     })
-    .then(pictures=>{
-        for(const pic of pictures){
+    .then(pictures => {
+        for (const pic of pictures) {
             const card = document.createElement('div');
+            card.className='card';
             card.innerHTML = `
-            `
+                <div class="photo">
+                    <img src= "${pic.url}" alt ="${pic.title}">
+                </div>
+                <p class="caption"> ${pic.title} </p>
+                <p class="date"> ${pic.date}  </p>
+                `;
+            gallery.appendChild(card);
         }
 
         console.log(pictures);
 
     })
-    .catch(error=>{
+    .catch(error => {
         console.error(error);
     })
-    .finally(()=>{
+    .finally(() => {
 
     });
