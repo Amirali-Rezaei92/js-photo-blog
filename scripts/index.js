9a6edd7203694c3a6c3425a84d5435baa26865a5
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-17 12:50:43 +0200

    Configurato fetch verso l’endpoint delle immagini, aggiunto ciclo for...of per preparare la generazione dinamica delle card e collegato correttamente il container .gallery senza elementi statici.

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,21 @@
 const API_URL = 'https://lanciweb.github.io/demo/api/pictures/'
 
-
+const gallery = document.querySelector('.gallery');
 
 
 fetch(API_URL)
     .then(response => {
         return response.json();
     })
-    .then(json=>{
-        console.log(json);
+    .then(pictures=>{
+        for(const pic of pictures){
+            const card = document.createElement('div');
+            card.innerHTML = `
+            `
+        }
+
+        console.log(pictures);
+
     })
     .catch(error=>{
         console.error(error);
